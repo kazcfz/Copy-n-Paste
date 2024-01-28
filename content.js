9d6da4f350bcfa8dc4416bc0c54429b8f3696d1e
Kaz <102762373@students.swinburne.edu.my>
2024-01-29 01:19:45 +0800

    content.js - Code and comment clean
    Too much restrictions on navigator.clipboard.read(), couldn't figure a working workaround yet.
    
    Tried:
    - Offscreen
    - External JS (kinda)
    - Service workers (https://bugs.chromium.org/p/chromium/issues/detail?id=1160302)

--- content.js
+++ content.js
@@ -8,20 +8,18 @@ function afterDOMLoaded(){
   fileInputs.forEach((input) => {
     input.addEventListener('click', handleFileInputClick);
   });
-  console.log("File input elements found:", fileInputs.length); //test
+  // console.log("File input elements found:", fileInputs.length);
 }
 
-// Function to handle click event on file input element
+// When input elements are clicked
 function handleFileInputClick(event) {
   event.preventDefault();
-  console.log('Input file clicked success');
   const originalInput = event.target;
 
   // Create overlay
   const overlay = document.createElement('div');
   overlay.classList.add('overlay');
 
-  // Load overlay content
   fetch(chrome.runtime.getURL('overlay.html'))
   .then(response => response.text())
   .then(html => {
@@ -47,6 +45,7 @@ function handleFileInputClick(event) {
         if (clipboardItems[0]['types'][0] == 'image/png') {
           clipboardItem.getType('image/png').then(blob => {
             const reader = new FileReader();
+
             reader.onload = function(event) {
               const imagePreview = overlay.querySelector('#image-preview');
               const img = document.createElement('img');
@@ -55,36 +54,34 @@ function handleFileInputClick(event) {
               img.style.maxHeight = '200px';
 
               img.addEventListener('click', () => {
-                // Convert the blob into a file object
+                // Convert blob into file object
                 const file = new File([blob], 'screenshot.png', { type: blob.type });
 
-                // Create a new FormData object
                 const fileList = new DataTransfer();
                 fileList.items.add(file);
-
                 originalInput.files = fileList.files;
                 
-                // // Trigger change event on original input to update its value
+                // Trigger change event on original input to update its value
                 const changeEvent = new Event('change', { bubbles: true });
                 originalInput.dispatchEvent(changeEvent);
-                // // Close the overlay
+                
                 closeOverlay()
               });
 
               imagePreview.appendChild(img);
             };
             reader.readAsDataURL(blob);
-          }).catch(error => {
-            console.log('%c📋 Paste Image Uploader:', 'font-weight: bold; font-size: 1.3em;',
-              '\nNo image found in clipboard');
-          }); //Never called because if condition is set already
+          });
         }
       });
-    }).catch(error => { /* pass */ });
+    }).catch(error => {
+      console.log('%c📋 Paste Image Uploader:', 'font-weight: bold; font-size: 1.3em;',
+        '\nFailed to read clipboard');
+    });
   });
 }
 
-// Function to convert blob to File object
+// Convert blob to File object
 function blobToFile(blob) {
     const file = new File([blob], 'clipboard-image.png', { type: blob.type });
     return file;
@@ -104,48 +101,14 @@ function closeOverlayOnClickOutside(event) {
     closeOverlay()
 }
 
-// Function to handle file selection in overlay and pass it to original input file element
+// Pass image selected in overlay, to original input file element
 function handleOverlayFileSelection(event, originalInput) {
   const file = event.target.files[0];
   originalInput.files = event.target.files;
 
-  // Trigger change event on original input to update its value
+  // Trigger change event on original input to update value (such as disabled buttons)
   const changeEvent = new Event('change', { bubbles: true });
   originalInput.dispatchEvent(changeEvent);
 
-  // Close the overlay
   closeOverlay()
 }
-
-
-
-
-/*
-
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'pasteImage') {
-    const image = message.imageData;
-    const previewDiv = document.getElementById('preview');
-    previewDiv.innerHTML = `<img src="${image}" onclick="pasteImageIntoInput('${image}')">`;
-  }
-});
-
-function pasteImageIntoInput(imageData) {
-  const fileInputs = document.querySelectorAll("input[type='file']");
-  fileInputs.forEach((input) => {
-    input.files = [dataURItoBlob(imageData)];
-  });
-}
-
-function dataURItoBlob(dataURI) {
-  const byteString = atob(dataURI.split(',')[1]);
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-  const ab = new ArrayBuffer(byteString.length);
-  const ia = new Uint8Array(ab);
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  return new Blob([ab], { type: mimeString });
-}
-*/
